19dd6edf568395e5516cf0060fd810d78a284313
sranik28~ <anikshariarrahman@gmail.com>
2023-02-13 04:21:07 +0600

    audio

--- js/deposit.js
+++ js/deposit.js
@@ -1,16 +1,18 @@
-document.getElementById('deposit-button').addEventListener('click', function(){
-   const depositInput = document.getElementById('input-deposit')
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const depositInput = document.getElementById('input-deposit')
     const input = depositInput.value;
     const newInput = parseFloat(input);
     depositInput.value = '';
-    if(isNaN(newInput)){
+    if (isNaN(newInput)) {
         alert('please valid a number')
         return;
     }
     if (newInput < 0) {
+        document.getElementById('myAudio').play();
         alert("Negative number entered!");
+        document.getElementById("myAudio").pause();
         return;
-      }
+    }
     const depositFilde = document.getElementById("deposit")
     const depositTotalElement = depositFilde.innerText;
     const newDeposittotalElement = parseFloat(depositTotalElement);
@@ -20,33 +22,42 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     const balanceTotalElement = balance.innerText;
     const newBalanceTotalElement = parseFloat(balanceTotalElement);
     const currentBalance = newBalanceTotalElement + newInput;
-    balance.innerText = currentBalance;    
+    balance.innerText = currentBalance;
 })
 
 // withdraw----------------
 
-document.getElementById('withdraw-button').addEventListener('click', function(){
+document.getElementById('withdraw-button').addEventListener('click', function () {
     const inputWithdraw = document.getElementById('input-withdraw');
     const inputWithdrawString = inputWithdraw.value;
     const newInputWithdraw = parseFloat(inputWithdrawString);
     inputWithdraw.value = '';
-    if(isNaN(newInputWithdraw)){
-        alert('please valid a number')
+    if (isNaN(newInputWithdraw)) {
+        document.getElementById('myAudio').play();
+        alert('please valid a number');
+        document.getElementById("myAudio").pause();
+        return;
+    }
+    if (newInputWithdraw < 0) {
+        document.getElementById('myAudio').play();
+        alert('please Enter A positive Number');
+        document.getElementById("myAudio").pause();
         return;
     }
     const withdraw = document.getElementById('withdraw');
     const withdrawTotalElementString = withdraw.innerText;
     const newWithdrawTotalElement = parseFloat(withdrawTotalElementString)
-    
-    
-    
+
     const withdrawBalance = document.getElementById('Balance');
     const withdrawTotalBalanceString = withdrawBalance.innerText;
     const newWithdrawTotalBalance = parseFloat(withdrawTotalBalanceString);
-    if(newInputWithdraw > newWithdrawTotalBalance){
-       alert("You have no money ,Please Deposit amount")
-       return ;
-    }
+    if (newInputWithdraw > newWithdrawTotalBalance) {
+        document.getElementById('myAudio').play();
+        alert("You have no money ,Please Deposit amount");
+        document.getElementById("myAudio").pause();
+        return;
+    }     
+    
     const currentWithdraw = newWithdrawTotalElement + newInputWithdraw;
     withdraw.innerText = currentWithdraw;
     const newCurrentBalance = newWithdrawTotalBalance - newInputWithdraw;
--- js/index.js
+++ js/index.js
@@ -5,9 +5,10 @@ document.getElementById("submit-button").addEventListener("click", function(){
    const userPass = document.getElementById("text-pass");
    const pass = userPass.value;
    if(email === "mdsranik2gmail.com" && pass === "anikAnik")
-   window.location.href = "bank.html";
+   window.location.href = "deposit.html";
 
    else{
    alert ('Dukte Demu Na Vag')
    }
 })
+
